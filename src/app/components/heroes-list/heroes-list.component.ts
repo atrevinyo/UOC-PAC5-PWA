312b67019f9ef066cbad4609dae6789546fb532c
atrevinyo <69076610+atrevinyo@users.noreply.github.com>
2021-12-28 12:04:04 +0100

    Add files via upload

--- src/app/components/heroes-list/heroes-list.component.ts
+++ src/app/components/heroes-list/heroes-list.component.ts
@@ -6,39 +6,19 @@ import {  trigger, state, transition, sequence, style, animate, query, stagger, 
 
 export const fadeOut =
   trigger('fadeOut', [
-    state('void', style({ background: 'pink', borderBottomColor: 'pink', opacity: 0, transform: 'translateX(-550px)', 'box-shadow': 'none' })),
+    state('void', style({ background: 'blue', borderBottomColor: 'blue', opacity: 0, transform: 'translateX(-550px)', 'box-shadow': 'none' })),
     transition('void => *', sequence([
-      animate(".5s ease")
+      animate(".5s ease-out")
     ])),
-    transition('* => void', [animate("5s ease")])
+    transition('* => void', [animate("5s ease-out")])
   ]);
 
-export const rowsAnimation =
-  trigger('rowsAnimation', [
-    transition('void => *', [
-      style({ height: '*', opacity: '0', transform: 'translateX(-550px)', 'box-shadow': 'none' }),
-      sequence([
-        animate(".35s ease", style({ height: '*', opacity: '.2', transform: 'translateX(0)', 'box-shadow': 'none' })),
-        animate(".35s ease", style({ height: '*', opacity: 1, transform: 'translateX(0)' }))
-      ])
-    ])
-  ]);
-
-
-
-export const blub =
-  trigger('blub', [
-    transition(':leave', [
-      style({ background: 'pink'}),
-      query('*', stagger(-150, [animateChild()]), { optional: true })
-    ]),
-  ]);
 
 @Component({
   selector: 'app-heroes-list',
   templateUrl: './heroes-list.component.html',
   styleUrls: ['./heroes-list.component.css'],
-  animations: [fadeOut, blub, rowsAnimation],
+  animations: [fadeOut],
 })
 export class HeroesListComponent implements OnInit {
 
